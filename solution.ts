419f1df5dffaab87f29fa3edef58db46fff8f4f3
SDNATH-Git <sd.nath1952@gmail.com>
2025-11-18 00:17:10 +0600

    solution problem 3

--- solution.ts
+++ solution.ts
@@ -34,7 +34,35 @@ function getLength (value : string | any[]): number{
  }
 }
 
-console.log(getLength('typescript'));
-console.log(getLength([10, 20, 30, 40]));
-console.log(getLength([]));
-console.log(getLength(''));
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
+// console.log(getLength([]));
+// console.log(getLength(''));
+
+
+//---- problem 3 -----//
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string{
+        return `'Name: ${this.name}, Age: ${this.age}';`;
+
+    }
+
+}
+
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
+
+'Name: John Doe, Age: 30';
+'Name: Alice, Age: 25';
